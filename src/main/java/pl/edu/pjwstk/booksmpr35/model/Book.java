750c586e0ffabe851ca066b44a9005f88ad56008
Bluelos1 <s25946@pjwstk.edu.pl>
2022-10-24 00:46:49 +0200

    end

--- src/main/java/pl/edu/pjwstk/booksmpr35/model/Book.java
+++ src/main/java/pl/edu/pjwstk/booksmpr35/model/Book.java
@@ -5,22 +5,24 @@ import pl.edu.pjwstk.booksmpr35.model.enums.BookType;
 import java.time.LocalDate;
 
 public class Book {
+    private Long id;
     private String title;
     private Author author;
     private BookType bookType;
-    private LocalDate publishedDate;
+    private LocalDate publishDate;
     private String publisher;
 
     public Book() {
     }
 
-    public Book(String title, Author author, BookType bookType, LocalDate publishedDate, String publisher) {
+    public Book(String title, LocalDate publishDate, BookType bookType, Author author) {
         this.title = title;
-        this.author = author;
+        this.publishDate = publishDate;
         this.bookType = bookType;
-        this.publishedDate = publishedDate;
-        this.publisher = publisher;
+        this.author = author;
     }
+
+
 
     public String getTitle() {
         return title;
@@ -35,10 +37,38 @@ public class Book {
     }
 
     public LocalDate getPublishedDate() {
-        return publishedDate;
+        return publishDate;
     }
 
     public String getPublisher() {
         return publisher;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public void setBookType(BookType bookType) {
+        this.bookType = bookType;
+    }
+
+    public void setPublishedDate(LocalDate publishedDate) {
+        this.publishDate = publishedDate;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
