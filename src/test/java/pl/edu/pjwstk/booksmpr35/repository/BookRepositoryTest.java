ed400a6ff242d76840fa581b58cc4960c6bfb894
Bluelos1 <s25946@pjwstk.edu.pl>
2022-10-23 23:55:05 +0200

    Initial commit

---
+++ src/test/java/pl/edu/pjwstk/booksmpr35/repository/BookRepositoryTest.java
@@ -0,0 +1,12 @@
+package pl.edu.pjwstk.booksmpr35.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
